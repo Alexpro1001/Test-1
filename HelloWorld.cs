7854e78cdf4714a35c6efde38d12e0703048aeaa
Alexpro1001 <alextoribioarroyos@gmail.com>
2024-12-20 20:11:47 +0100

    Afegir nou arxiu

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //S'executa 1 cop al principi
+      print("Paguero");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //S'executa cada frame
+      print("eres gey i eres mas tonto que la polla");  
+    }
+}
